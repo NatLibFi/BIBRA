136b657eb322a542b055d08918f3c6c4a3f8f644
Osma Suominen <osma.suominen@helsinki.fi>
2026-03-26 16:55:53 +0200

    chore: disable video recording in Cypress configuration

--- cypress.config.js
+++ cypress.config.js
@@ -6,6 +6,6 @@ module.exports = defineConfig({
     specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
     supportFile: 'cypress/support/e2e.js',
     screenshotOnRunFailure: true,
-    video: true,
+    video: false,
   },
 });
